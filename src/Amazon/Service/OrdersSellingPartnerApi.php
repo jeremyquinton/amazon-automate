fd6f70549f55cb5eac86ab41442d31250077037c
Jeremy Quinton <jeremyquinton@gmail.com>
2025-02-19 14:15:38 +0200

    correct the barcodes of previous orders to be the new barcode

--- src/Amazon/Service/OrdersSellingPartnerApi.php
+++ src/Amazon/Service/OrdersSellingPartnerApi.php
@@ -19,6 +19,43 @@ class OrdersSellingPartnerApi
 
     private $ordersToOrderItems;
 
+    //6009714350580
+    private $oldToNewSku = ['33-AMWN-QJ58' => '9902059200015',
+                            '7Y-2MVT-F1WD' => '9902184170788',
+                            '9H-9FI9-1CQI' => '9902164116805',
+                            'BC-A1Y5-XF2H' => '9900964646911',
+                            'BO-56VC-1L6V' => '9902182412477',
+                            'OQ-S286-Y0CD' => '9902184919844',
+                            'Q8-RAFZ-R0W9' => '9901038524234',
+                            'S1-4PVD-2LOK' => '9902185223315',
+                            'S8-PNIN-HAXN' => '9902031717920',
+                            'TM-IYOM-GEDL' => '9902183941686',
+                            'UC-NCYR-WZ6A' => '9902041198764',
+                            'UV-A1R7-Y48I' => '9902141892708',
+                            'V7-Q5D8-E1D0' => '9902134249472',
+                            'VM-6LU7-J8N9' => '9902186424292',
+                            'XU-OT8W-M8WJ' => '9902175696570',
+                            'Y1-2KH8-ERGT' => '9902191777567',
+                            'YH-8HXY-2R31' => '9900959435902',
+                            'ZP-SVF0-395M' => '9901080708408',
+                            '0B-U0VE-WTN6' => '9901058590721',
+                            '2Y-ZS5R-7551' => '9901038117924',
+                            '4S-F81Y-JBH7' => '9902080318437',
+                            'AO-2JBR-84XW' => '9902134557997',
+                            'B7-7KZ6-CJFQ' => '9901058590929',
+                            'CW-YTKK-6DZ1' => '9900962027958',
+                            'DW-L4G6-GKR0' => '9902018451434',
+                            'HR-7H66-FA6Q' => '9901038814403',
+                            'JA-SE4O-0THT' => '9902134559809',
+                            'JQ-Z5MS-GK4N' => '9902041198955',
+                            'MF-NP2Q-1SNS' => '9901013173143',
+                            'NQ-L00C-1W8O' => '9902044242365',
+                            'NR-C5DS-QUV8' => '9902080318277',
+                            'O8-UAU6-6KIW' => '9902184271850',
+                            'OP-FR8M-UTQK' => '9902062631042',
+                            'PZ-N1TP-OI27' => '9902027182909',
+                            'UJ-7O2I-ZERL' => '9902184269741']; 
+
     public function __construct(EntityManagerInterface $entityManager,Config $config)
     {
 
@@ -156,6 +193,11 @@ class OrdersSellingPartnerApi
 
             sleep(2);
         } 
+        //object is immutable so have to just fix database with series of sql queries
+        foreach ($this->oldToNewSku as $oldSku => $newSku) {
+            $query = "update amazonOrderItems set sellerSku='" . $newSku ."' where sellerSku='" . $oldSku . "'";
+            $this->entityManager->getConnection()->prepare($query)->executeQuery();
+        }
     }
 
     public function processOrdersToDb() {
